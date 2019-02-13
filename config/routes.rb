22877a76f9393fd8a688f33faf6f5607028ac479
Gan Tu <tugan@berkeley.edu>
2019-02-13 13:09:35 -0800

    use paths, instead of hard-coded urls, in partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/contact'
+  get  '/help',    to: 'static_pages#help'
+  # tutorial: can also rename it using 'as'
+  #     For example:
+  # get  '/help',    to: 'static_pages#help', as: "haha" ==> haha_path
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
