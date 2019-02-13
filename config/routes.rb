45115e7e533bb287a8fe429e7294b19af6841a41
Gan Tu <tugan@berkeley.edu>
2019-02-13 13:30:30 -0800

    add signup page and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
+
   # tutorial: can also rename it using 'as'
   #     For example:
   # get  '/help',    to: 'static_pages#help', as: "haha" ==> haha_path
+  
+  get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+
+  get "/signup", to: 'users#new'
 end
