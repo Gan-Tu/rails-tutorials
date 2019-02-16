9dca20c489e735c880f7ec9f2a11714d39a24a7f
Gan Tu <tugan@berkeley.edu>
2019-02-16 11:41:48 -0800

    generate session controller skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
 
   # tutorial: can also rename it using 'as'
   #     For example:
   # get  '/help',    to: 'static_pages#help', as: "haha" ==> haha_path
   
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-
-  get "/signup",   to: 'users#new'
-  post '/signup',  to: 'users#create'
+  get     '/help',        to: 'static_pages#help'
+  get     '/about',       to: 'static_pages#about'
+  get     '/contact',     to: 'static_pages#contact'
+  get     '/signup',      to: 'users#new'
+  post    '/signup',      to: 'users#create'
+  get     '/login',       to: 'sessions#new'
+  post    '/login',       to: 'sessions#create'
+  delete  '/logout',      to: 'sessions#destroy'
 
   resources :users
 end
