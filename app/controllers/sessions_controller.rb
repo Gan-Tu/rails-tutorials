b44d9edc3143fa309dc92b7d2f334e6fe7fcb03e
Gan Tu <tugan@berkeley.edu>
2019-02-16 12:55:23 -0800

    redirect login page upon authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
-            # log in
+            log_in(user) # or log_in user
+            redirect_to user_url(user) # or: redirect_to user
         else
             flash.now[:danger] = "Invalid email/password combination"
             render "new"
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+
+    # Logs in the given user
+    def log_in(user)
+        # Because temporary cookies created using the session method are
+        # automatically encrypted, the code is secure
+        # This applies only to temporary sessions initiated with the session 
+        # method, though, and is not the case for persistent sessions created
+        # using the cookies method
+        session[:user_id] = user.id
+    end
 end
