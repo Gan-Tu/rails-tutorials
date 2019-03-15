466aa93c8059710f9b65c7de23d2dcd02666cb3f
Gan Tu <tugan@berkeley.edu>
2019-03-14 18:02:50 -0700

    require edits and updates to have correct user logged in; refactor method names to be more expressive

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
-    before_action :logged_in_user , only: [:edit, :update]
+    before_action :ensure_user_logged_in, only: [:edit, :update]
+    before_action :ensure_correct_user,   only: [:edit, :update]
 
     def show
         @user = User.find(params[:id]) 
@@ -11,7 +12,7 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(user_params)
+        @user = User.new(safe_user_params)
         if @user.save
             log_in @user
             flash[:success] = "Welcome to the Sample App"
@@ -23,12 +24,10 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
-        if @user.update_attributes(user_params)
+        if @user.update_attributes(safe_user_params)
           # Handle a successful update.
             flash[:success] = "Profile updated"
             redirect_to @user
@@ -39,7 +38,7 @@ class UsersController < ApplicationController
 
     private
 
-        def user_params
+        def safe_user_params
             return params.require(:user).permit(:name, :email, :password,
                                                 :password_confirmation)
         end
@@ -47,10 +46,21 @@ class UsersController < ApplicationController
         ######## Before filters ########
 
         # Confirms a logged-in user.
-        def logged_in_user
+        def ensure_user_logged_in
           unless logged_in?
             flash[:danger] = "Please log in."
             redirect_to login_url
           end
+        end
+
+        # Returns true if the given user is the current user.
+        def is_current_user?(user)
+        user == current_user
+        end
+
+        # Confirms the correct user.
+        def ensure_correct_user
+          @user = User.find(params[:id])
+          redirect_to(root_url) unless is_current_user?(@user)
         end
 end
