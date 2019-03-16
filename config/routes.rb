3abba6f00b6c67ea9cd50289adabfe5c6f72f43a
Gan Tu <tugan@berkeley.edu>
2019-03-16 16:13:02 -0700

    add account activation controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,16 @@ Rails.application.routes.draw do
   delete  '/logout',      to: 'sessions#destroy'
 
   resources :users
+  # automatically generates:
+  #     users GET    /users(.:format)                        users#index
+  #           POST   /users(.:format)                        users#create
+  #  new_user GET    /users/new(.:format)                    users#new
+  # edit_user GET    /users/:id/edit(.:format)               users#edit
+  #      user GET    /users/:id(.:format)                    users#show
+  #           PATCH  /users/:id(.:format)                    users#update
+  #           PUT    /users/:id(.:format)                    users#update
+  #           DELETE /users/:id(.:format)                    users#destroy
+  
+  resources :account_activations, only: [:edit]
+
 end
