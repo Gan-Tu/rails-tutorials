13793e68925abba82d31553d31727a364ef5a5a5
Gan Tu <tugan@berkeley.edu>
2019-03-16 19:24:51 -0700

    create resources for password resets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
 
   # tutorial: can also rename it using 'as'
@@ -26,5 +30,6 @@ Rails.application.routes.draw do
   #           DELETE /users/:id(.:format)                    users#destroy
   
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
 end
