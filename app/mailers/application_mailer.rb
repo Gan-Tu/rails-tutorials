71b811a3ef88a4aac52c72c530b7cce2e9aa2745
Gan Tu <tugan@berkeley.edu>
2019-04-08 13:24:27 -0700

    use mailgun, instead of sendgrid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "noreply@tugan.io"
+  default from: "Rails Tutorial Sample App <noreply@mailgun.tugan.io>",
+          reply_to: "sample-app-reply@tugan.io"
   layout 'mailer'
 end
