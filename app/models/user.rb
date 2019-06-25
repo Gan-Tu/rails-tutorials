1e8c49b42ef5fe8dfe3e4465f70e229dea707c94
Gan Tu <tugan@berkeley.edu>
2019-06-25 13:42:50 -0700

    fix typo and spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,25 +5,25 @@ class User < ApplicationRecord
     before_create :create_activation_digest
 
     # equivalent: validates(:name, presence: true)
-    validates :name,    presence: true, 
+    validates :name,    presence: true,
                         length: { maximum: 50}
-    
+
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email,   presence: true, 
+    validates :email,   presence: true,
                         length: { maximum: 255},
                         format: { with: VALID_EMAIL_REGEX },
                         # if we use "uniqueness: true"
                         # it will be case sensitive, while it shouldn't be for email
                         uniqueness: { case_sensitive: false }
-    
+
     # for more: https://goo.gl/wLvfto
     # - automatically add 'password_confirmation', 'authenticate' method
     # - Adds methods to set and authenticate against a BCrypt password. T
     # his mechanism requires you to have a password_digest attribute
-    # - Validations for presence of password on create, confirmation of 
-    # password (using a password_confirmation attribute) are 
-    # automatically added. 
-    has_secure_password 
+    # - Validations for presence of password on create, confirmation of
+    # password (using a password_confirmation attribute) are
+    # automatically added.
+    has_secure_password
 
     validates :password, presence: true, # ensures non-empty
                          length: { minimum: 6 },
@@ -31,7 +31,7 @@ class User < ApplicationRecord
 
     # Return the hash digest of the given string
     def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? 
+        cost = ActiveModel::SecurePassword.min_cost ?
                     BCrypt::Engine::MIN_COST : BCrypt::Engine::cost
         return BCrypt::Password.create(string, cost: cost)
     end
@@ -51,7 +51,7 @@ class User < ApplicationRecord
     # # Returns false if user digest is nil (meaning no persistent login enabled)
     # def authenticated?(remember_token)
     #     # built-in for checking remember_digest == digest(remember_token)
-    #     return !remember_digest.nil? && 
+    #     return !remember_digest.nil? &&
     #             BCrypt::Password.new(self.remember_digest)
     #                             .is_password?(remember_token)
     # end
@@ -72,7 +72,7 @@ class User < ApplicationRecord
         update_attribute(:remember_digest, nil)
     end
 
-    # Activate an acccount
+    # Activate an account
     def activate
         update_columns(activated: true,
                        activated_at: Time.zone.now)
@@ -116,4 +116,3 @@ class User < ApplicationRecord
             self.activation_digest = User.digest(activation_token)
         end
 end
-    
