e2366c721a27a7e24c248301e720c00aab4f853b
Gan Tu <tugan@berkeley.edu>
2019-06-25 14:16:37 -0700

    destroy microposts of a user, when the user is deleted

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  
+
     def setup
-        @user = User.new(   name: "Example User", 
+        @user = User.new(   name: "Example User",
                             email: "user@example.com",
                             password: "foobar",
                             password_confirmation: "foobar")
@@ -86,6 +86,14 @@ class UserTest < ActiveSupport::TestCase
     test "authenticated? should return false for a user with nil digest" do
         # assert_not @user.authenticated?('')
         assert_not @user.authenticated?(:remember, '')
+    end
+
+    test "associated microposts should be destroyed" do
+        @user.save
+        @user.microposts.create!(content: "Lorem ipsum")
+        assert_difference 'Micropost.count', -1 do
+          @user.destroy
+        end
     end
 
 end
